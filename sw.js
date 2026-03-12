16e5da36974d072e2b5dcc20f96db596c0c1b5d1
RDPlayer16 <raydias.95k@gmail.com>
2026-03-12 12:02:02 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'wifi-pro-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.8/html5-qrcode.min.js'
+];
+
+// Instala o motor e baixa tudo (HTML e as bibliotecas)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Quando o app pede algo, o motor entrega o que tá salvo na memória em vez de usar a internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
